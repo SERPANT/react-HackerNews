44cfc4fec3c298cef17c5db17ae4a2ab4021a33f
Shreejit <shreejit955@gmail.com>
2018-11-07 15:46:58 +0545

    Updates

---
+++ precache-manifest.9bef057d6fdee153a239f7fc42f8cbce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "473569b1a57d1707bbb7",
+    "url": "/react-HackerNews/static/js/runtime~main.473569b1.js"
+  },
+  {
+    "revision": "c309f0f124d75831b95e",
+    "url": "/react-HackerNews/static/js/main.c309f0f1.chunk.js"
+  },
+  {
+    "revision": "71378186901a3f4d18eb",
+    "url": "/react-HackerNews/static/js/1.71378186.chunk.js"
+  },
+  {
+    "revision": "c309f0f124d75831b95e",
+    "url": "/react-HackerNews/static/css/main.9e8560ce.chunk.css"
+  },
+  {
+    "revision": "3281f3141cdc011971dccc115ed30fce",
+    "url": "/react-HackerNews/index.html"
+  }
+];
